2ae209695045bd6f3c83a612460b793f67372710
vwelch <vwelch>
1997-10-14 18:22:29 +0000

    Comment fixes.

--- krb525.h
+++ krb525.h
@@ -1,7 +1,7 @@
 /*
  * krb525 include file
  *
- * $Id: krb525.h,v 1.2 1997/09/15 15:37:44 vwelch Exp $
+ * $Id: krb525.h,v 1.3 1997/10/14 18:22:30 vwelch Exp $
  *
  */
 
@@ -11,6 +11,10 @@
 #define KRB525_SERVICE		"krb525"
 
 #define KRB525_PORT		6565
+
+/*
+ * Version string for sendauth/recvauth
+ */
 #define KRB525_VERSION		"KRB525 v1.0"
 
 #define	STATUS_OK		0
